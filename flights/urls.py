0c4e0514fdd7f25a50c0455150f5571cc2e0b337
ahn <sungjoonan02@gmail.com>
2022-03-18 16:00:41 +0900

    항공편/해성 조회 view 기능 수정4

--- flights/urls.py
+++ flights/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from flights.views import PlanetListView
+from flights.views import PlanetListView, FlightListView
 
 urlpatterns = [
-    path('/planet', PlanetListView.as_view())
+    path('/planet', PlanetListView.as_view()),
+    path('/schedule', FlightListView.as_view())
 ]
